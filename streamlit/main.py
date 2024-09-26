6afca9df0dfc699628651d5105597f6b39c1edda
LouieGGG <lou.albalat.93@gmail.com>
2024-09-26 12:10:55 +0200

    Creación de barra lateral
    -Creación barra lateral básica.
    -get_balance_xxxxx() por determinar, solo está puesto por poner.
    
    Co-Authored-By: Harillo <140201668+HarilloP@users.noreply.github.com>

--- streamlit/main.py
+++ streamlit/main.py
@@ -9,6 +9,31 @@ def main():
     st.title('REData API')
     st.text('Datos del cuadro de mandro de la red eléctrica de España')
 
+
+    #Barra lateral
+    sidebar_opciones = ['Balance', 'Demanda', 'Generación', 'Intercambio']
+    selected_option = st.sidebar.selectbox('Seleccione una opción', sidebar_opciones)
+
+    # Llamada a la API
+
+    ### get_balance_data, get_balance_data, get_generacion_data, get_intercambio_data por determinar.
+    if selected_option == 'Balance':
+        balance_data = get_balance_data()
+        st.subheader('Balance de energía eléctrica')
+        st.dataframe(balance_data)
+
+    elif selected_option == 'Demanda':
+        demand_data = get_demanda_data()
+        st.subheader('Demanda corregida')
+        st.dataframe(demand_data)
+
+    elif selected_option == 'Generación':
+        generation_data = get_generacion_data()
+        st.subheader('Generación')
+        st.dataframe(generation_data)
+
+    elif selected_option == 'Intercambio':
+        exchange_data = get_intercambio_data
     pass
 
 if __name__ == "__main__":
