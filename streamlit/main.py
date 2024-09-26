b897a181c307dc4d7b3ded520692599a56dcce78
LouieGGG <lou.albalat.93@gmail.com>
2024-09-26 11:32:36 +0200

    Creación Streamlit main
    Creación inicial de main.py de streamlit
    
    Co-Authored-By: Harillo <140201668+HarilloP@users.noreply.github.com>

---
+++ streamlit/main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+import requests
+
+def main():
+
+    st.title('REData API')
+    st.text('Datos del cuadro de mandro de la red eléctrica de España')
+
+    pass
+
+if __name__ == "__main__":
+    main()
