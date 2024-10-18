e29e412b3b0914a790be7036d32dea5de65de48e
LouieGGG <lou.albalat.93@gmail.com>
2024-10-18 13:27:57 +0200

    correccion

--- streamlit/vista/home.py
+++ streamlit/vista/home.py
@@ -7,30 +7,6 @@ import streamlit as st
 
 def inicio_app():
 
-    st.image('../../source/vector.png')
-    st.markdown(
-    """
-    <style>
-      .background-image {
-        background-image: url('https://drive.google.com/file/d/1eed4avDOFutJSjMEuUnRBL5amrRccU2I/view?usp=sharing');
-        background-repeat: no-repeat;
-        background-position: top right;
-        background-size: contain; /* Adjust size as needed */
-        position: fixed; /* Keep it in place */
-        top: 0;
-        right: 0;
-        width: 200px; /* Adjust width as needed */
-        height: 100px; /* Adjust height as needed */
-        z-index: -1; /* Ensure it stays behind other content */
-      }
-    </style>
-
-    <div class="background-image"></div>
-    """,
-    unsafe_allow_html=True
-)
-
-
     #Titulo
 
     st.markdown("<h1 class='center-font; color: blue;'>Proyecto cuadro de mando de la red eléctrica de España</h1>", unsafe_allow_html=True)
