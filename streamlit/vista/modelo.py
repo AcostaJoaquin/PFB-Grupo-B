2823b2252424143518954ba2b25b018093f75543
diegodiazgomez <gdiegodiazg@gmail.com>
2024-10-21 12:35:37 +0200

    actualización modelo.py

--- streamlit/vista/modelo.py
+++ streamlit/vista/modelo.py
@@ -1,21 +1,12 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-
-import matplotlib.pyplot as plt
 import plotly.express as px
 
 import pickle
 from tensorflow.keras.models import load_model
-from keras.models import Sequential
-from keras.layers import Input, Dense, SimpleRNN, Dropout, LSTM
-from keras.optimizers import Adam
-from sklearn.preprocessing import MinMaxScaler
-
-import statsmodels
 import os
-from datetime import datetime, timedelta
-from .connection import demanda_datos
+
 
 
 def get_demanda_data():
@@ -42,10 +33,18 @@ def main():
     st.markdown(body = """En este apartado veremos las predicciones realizadas por el modelo 
                           de Machine Learning que hemos entrenado""")
     
-    años = [2023, 2024]
-    año = st.selectbox(label = "Elige este año o el anterior para la proyección de datos", options = años)
 
-    días = [4,5,6,7,8,9,10,11,12,13,14]
+    st.markdown(body = """Se mostrará la predicción de tantos días como se indique a continuación del último día 
+                       de actualización de la página, o de sus equivalentes en años anteriores, 
+                       si se ha seleccionado otro año.""")
+    st.markdown(body = """Tarda unos segundo en cargar, y si se cambian los parámetros 
+                       hay que volver a esperar unos segundos.""")
+
+
+    años = [2024, 2023, 2022]
+    año = st.selectbox(label = "Elige año para la proyección de datos", options = años)
+
+    días = [2,3,4,5,6,7,8,9,10,11,12,13,14]
     n_días = st.selectbox(label  = "Di cuántos días quieres predecir",
                               options = días) 
 
@@ -76,7 +75,7 @@ def main():
 
     while len(validation_predictions) <= n_días:
         
-        # En la primera iteración predice el siguiente valor de usando X
+        # En la primera iteración predice el siguiente valor usando X
         # En las siguientes iteraciones usa el valor predicho anterior para predecir el siguiente
         p = modelo.predict(last_x.reshape(1, -1, 1))[0, 0]
         
@@ -91,12 +90,10 @@ def main():
     validation_predictions = escalador.inverse_transform(validation_predictions)
     validation_predictions = pd.DataFrame(validation_predictions, columns = ["valores"])
 
-    #st.pyplot(validation_predictions)
-    
+    #Graficamos las predicciones    
     fig_pred = px.line(data_frame= validation_predictions,
             y = "valores")
-
-    #fig_pred.update_layout(title = 'Predicción de la evolución de la demanda')
+    fig_pred.update_layout(title = 'Predicción de la evolución de la demanda')
     st.plotly_chart(figure_or_data = fig_pred,
                 use_container_width = True)
 
